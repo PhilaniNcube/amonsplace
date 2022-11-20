79719c30fb03d49fa2bb9ee61bfcb67a5499cbd3
Philani Ncube <ncbphi001@gmail.com>
2022-11-20 17:43:25 +0200

    new hero image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,19 +19,32 @@ const Navbar = ({setOpen, open}: {setOpen: (arg0:boolean) => void, open:boolean}
 
   ]
 
-  return <header className="py-4 md:py-8 px-4">
-    <div className="max-w-7xl mx-auto flex justify-between items-center">
-      <Image src="/images/logo-01.svg" width={494} height={134} alt="logo" className=" w-40 md:w-56 object-cover" />
+  return (
+    <header className="py-4 md:py-8 px-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <Image
+          src="/images/logo-01.svg"
+          width={494}
+          height={134}
+          alt="logo"
+          className=" w-40 md:w-56 object-cover"
+        />
 
-      <nav className="space-x-4 text-black uppercase hidden">
-        {links.map((link, i) => (
-          <Link key={i} href={link.href} className="text-lg font-medium">{link.title}</Link>
-        ))}
-      </nav>
-      <button onClick={() => setOpen(true)} className="bg-black tracking-wider text-white font-medium uppercase px-8 py-1 min-w-fit max-w-xl">
-        Book Now
-      </button>
-    </div>
-  </header>;
+        <nav className="space-x-4 text-black uppercase hidden">
+          {links.map((link, i) => (
+            <Link key={i} href={link.href} className="text-lg font-medium">
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+        <button
+          onClick={() => setOpen(true)}
+          className="bg-[#a0916d] tracking-wider text-white rounded-full font-medium uppercase px-8 py-1 min-w-fit max-w-xl"
+        >
+          Book Now
+        </button>
+      </div>
+    </header>
+  );
 };
 export default Navbar;
